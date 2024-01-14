38b00ee178919cf1c5c5c6401d1ed804bf8785d3
Khalidii <khalidiiahmed964@gmail.com>
2024-01-14 21:21:24 +0300

    my third commit

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("First line");
         System.out.println("second line ");
+        System.out.println("third line ");
 
 
 
